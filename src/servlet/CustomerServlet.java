fa4bd810f9b7e29c16bccc702cf467487ba10dbb
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-05-12 13:11:27 +0530

    'Update index.html'

--- src/servlet/CustomerServlet.java
+++ src/servlet/CustomerServlet.java
@@ -1,9 +1,60 @@
 package servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            resp.setContentType("application/json");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/WebSuperMarket", "root", "1234");
+            PrintWriter writer = resp.getWriter();
+
+            ResultSet rst = connection.prepareStatement("SELECT * FROM Customer").executeQuery();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+            while (rst.next()){
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                String contact = rst.getString(4);
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("id",id);
+                objectBuilder.add("name",name);
+                objectBuilder.add("address",address);
+                objectBuilder.add("contact",contact);
+
+                arrayBuilder.add(objectBuilder.build());
+            }
+
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 200);
+            response.add("message", "Done");
+            response.add("data", arrayBuilder.build());
+            writer.print(response.build());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
 }
