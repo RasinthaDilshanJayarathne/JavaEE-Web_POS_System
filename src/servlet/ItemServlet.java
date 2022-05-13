62baab173b69022aee76fb5ad31950d5535a8486
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-05-13 06:48:06 +0530

    'Get & Show All Customers Data in Item Table'

--- src/servlet/ItemServlet.java
+++ src/servlet/ItemServlet.java
@@ -1,9 +1,49 @@
 package servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
 
+            resp.setContentType("application/json");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/WebSuperMarket", "root", "1234");
+            ResultSet rst = connection.prepareStatement("SELECT * FROM Item").executeQuery();
+            String allRecodes="";
+
+            while (rst.next()){
+                String code = rst.getString(1);
+                String name = rst.getString(2);
+                String price = rst.getString(3);
+                String qtyOnHand = rst.getString(4);
+                System.out.println(code+" "+name+" "+price+" "+qtyOnHand);
+
+                String customer = "{\"code\":\"" + code + "\",\"name\":\"" + name + "\",\"price\":\"" + price + "\",\"qtyOnHand\":\"" + qtyOnHand + "\"},";
+                allRecodes = allRecodes + customer;
+            }
+
+            String finalJson = "[" + allRecodes.substring(0, allRecodes.length() - 1) + "]";
+            PrintWriter writer = resp.getWriter();
+            writer.write(finalJson);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
 }
